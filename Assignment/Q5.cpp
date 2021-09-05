46f05d75bd5c9c32168055aa507811787d8fbd51
Ramya002 <87769452+Ramya002@users.noreply.github.com>
2021-09-05 09:29:08 +0530

    Update Q5.cpp

--- Assignment/Q5.cpp
+++ Assignment/Q5.cpp
@@ -1,1 +1,29 @@
+// Write a program to check whether a number is Prime or Not
 
+#include<iostream>
+using namespace std;
+int main(){
+	int n,i;
+	bool p = true;
+
+	cout<<"Enter a natural number: ";
+	cin>>n;
+	if(n==0 || n==1){
+		cout<<n<<" is not a prime number";
+	}
+	else{
+		for (i = 2; i <= n / 2; i++){
+            if (n % i == 0) {
+                p = false;
+                break;
+            }
+		}
+	}
+	if(p){
+		cout<<n<<" is a prime number";
+	}
+	else{
+		cout<<n<<" is not a prime number";
+	}
+	return 0;
+}
