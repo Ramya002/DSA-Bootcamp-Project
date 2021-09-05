8a10c3527b341b5f7bae6964a7e823ff66c889a3
Ramya002 <87769452+Ramya002@users.noreply.github.com>
2021-09-05 09:10:37 +0530

    Update Q3.cpp

--- Assignment/Q3.cpp
+++ Assignment/Q3.cpp
@@ -1,1 +1,22 @@
+//  Write a program to check whether a year entered by a user is Leap year or not
 
+#include<iostream>
+using namespace std;
+int main(){
+	int y;
+	cout<<"Enter a year: ";
+	cin>>y;
+	if (y % 4 == 0) {
+        if (y % 100 == 0) {
+            if (y % 400 == 0)
+                cout << y << " is a leap year";
+            else
+                cout << y << " is not a leap year";
+        }
+        else
+            cout << y << " is a leap year";
+    }
+    else
+        cout << y << " is not a leap year";
+	return 0;
+}
