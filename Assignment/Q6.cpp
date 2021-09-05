55c8db8b0c7d9cc07715fc8eb952b0f5ffd2e6f5
Ramya002 <87769452+Ramya002@users.noreply.github.com>
2021-09-05 09:37:49 +0530

    Update Q6.cpp

--- Assignment/Q6.cpp
+++ Assignment/Q6.cpp
@@ -1,1 +1,24 @@
+/* Print this pattern using loops
+For n=5
+	    *
+	   * *
+	  * * *
+	 * * * *
+	* * * * *
+*/
 
+#include<iostream>
+using namespace std;
+int main(){
+	int n = 5, s, i, j;
+	for(i = 1; i <= n; i++){
+		for(s = i; s < n; s++){
+			cout<<" ";
+		}
+		for(j = 1; j <= i; j++){
+			cout<<"* ";	
+		}
+		cout<<endl;
+	}
+	return 0;
+}
