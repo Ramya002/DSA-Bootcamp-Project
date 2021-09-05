9a9dbe965fe7b83a2a7dc2d66006bb76f0375aeb
Ramya002 <87769452+Ramya002@users.noreply.github.com>
2021-09-05 11:27:16 +0530

    Update Q10.cpp

--- Assignment/Q10.cpp
+++ Assignment/Q10.cpp
@@ -1,1 +1,38 @@
+// CamelCase Hackerrank
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int camelcase(string s) {
+    int ans=0;
+    for(int i=0;i<s.size();i++){
+        if(isupper(s[i])){
+            ans++;   
+        }
+    }
+    return ans+1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
