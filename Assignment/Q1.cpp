5cc4eac1af283f2b87a835220a34a9ca10eaa31c
Ramya002 <87769452+Ramya002@users.noreply.github.com>
2021-09-05 08:51:01 +0530

    Update Q1.cpp

--- Assignment/Q1.cpp
+++ Assignment/Q1.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int a = 2, b = 5, temp;
 
+    cout << "Before swapping the numbers are: " << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping the numbers are: " << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+	return 0;
+}
